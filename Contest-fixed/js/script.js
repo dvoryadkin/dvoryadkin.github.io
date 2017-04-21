9adbab854357f4f8569fe66bb048947396816657
dvoryadkin <dvoryadkin.nv@gmail.com>
2017-04-21 21:52:57 +0300

    del

--- Contest-fixed/js/script.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	var offset = $(".sidebar").offset();
-	var topPadding = 0;
-	$(window).scroll(function() {
-		if ($(window).scrollTop() > offset.top) {
-			$(".sidebar").stop().animate({marginTop: $(window).scrollTop() - offset.top + topPadding});
-		}
-		else {$(".sidebar").stop().animate({marginTop: 0});};});
-});
